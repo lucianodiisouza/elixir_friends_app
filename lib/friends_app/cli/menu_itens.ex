9ef1bf562f730ad37e6a4f4eb70ccc16975286eb
Luciano dii Souza <lucianodiisouza@hotmail.com>
2022-07-22 19:01:57 -0300

    running mix format (add it to a pre-commit script)

--- lib/friends_app/cli/menu_itens.ex
+++ lib/friends_app/cli/menu_itens.ex
@@ -1,10 +1,11 @@
 defmodule FriendsApp.CLI.MenuItems do
   alias FriendsApp.CLI.Menu
 
-  def all, do: [
-      %Menu{ label: 'Add a friend', id: :create },
-      %Menu{ label: 'Show a friend', id: :show },
-      %Menu{ label: 'Update a friend', id: :update },
-      %Menu{ label: 'Delete a friend', id: :delete },
-   ]
+  def all,
+    do: [
+      %Menu{label: 'Add a friend', id: :create},
+      %Menu{label: 'Show a friend', id: :show},
+      %Menu{label: 'Update a friend', id: :update},
+      %Menu{label: 'Delete a friend', id: :delete}
+    ]
 end
